f123c94174a89f81739b412ff753d655dc813b87
122-63 <3469@holbertonschool.com>
2022-06-14 23:46:43 -0500

    Hypermedia pagination

---
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+""" Simple pagination """
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page, page_size):
+    """
+        Range of the page
+        Args:
+            page: Current page
+            page_size: Total size of the page
+        Return:
+            tuple with the range start and end size page
+    """
+
+    if page and page_size:
+        start_size = (page - 1) * page_size
+        final_size = start_size + page_size
+        return start_size, final_size
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+            Get the page
+            Args:
+                page: Current page
+                page_size: Total size of the page
+            Return:
+                List of the pagination done
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        range: Tuple = index_range(page, page_size)
+        pagination: List = self.dataset()
+
+        return (pagination[range[0]:range[1]])
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """
+            returns a dictionary
+        """
+
+        data = []
+        try:
+            data = self.get_page(page, page_size)
+        except AssertionError:
+            return {}
+
+        dataset: List = self.dataset()
+        total_pag: int = len(dataset) if dataset else 0
+        total_pag = ceil(total_pag / page_size)
+        prev_pag: int = (page - 1) if (page - 1) >= 1 else None
+        next_pag: int = (page + 1) if (page + 1) <= total_pag else None
+
+        hypermedia: Dict = {
+            'page_size': page_size,
+            'page': page,
+            'data': data,
+            'next_page': next_pag,
+            'prev_page': prev_pag,
+            'total_pages': total_pag,
+        }
+
+        return hypermedia
