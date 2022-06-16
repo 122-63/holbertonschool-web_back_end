32464857aae3195acecae04931719c62fa82359b
122-63 <3469@holbertonschool.com>
2022-06-16 15:15:24 -0500

    FIFO caching

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -7,6 +7,7 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
+    """ Class that inherits from BaseCaching and is a caching system """
 
     def __init__(self):
         """ Init"""
