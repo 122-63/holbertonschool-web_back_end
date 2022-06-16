5f47fb914bb2459b263909ccb84269c2289ab058
122-63 <3469@holbertonschool.com>
2022-06-16 15:04:43 -0500

    Basic dictionary

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -14,7 +14,7 @@ class BasicCache(BaseCaching):
           the item value for the key key.
         * If key or item is None, this method should not do anything.
         """
-        if key or item is not None:
+        if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
