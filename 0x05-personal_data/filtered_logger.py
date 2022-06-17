fc532fc26ed6ed07505b5beb348f062322974368
122-63 <3469@holbertonschool.com>
2022-06-17 00:45:21 -0500

    pep8 blank lines

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -8,6 +8,7 @@ import re
 from typing import List
 import logging
 
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
@@ -32,16 +33,25 @@ class RedactingFormatter(logging.Formatter):
         return filter_datum(self.__fields, self.REDACTION,
                             message, self.SEPARATOR)
 
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """Returns an obfuscated message given its fields and its redaction.
-    Args:
-        fields: The fields of the message to be redacted.
-        redaction: The redaction of each field's value.
-        message: The message to be redacted.
-        separator: The separator character between each field.
-    Returns:
-        The message redacted.
+    """
+        Filter and obfuscated the string
+        Args:
+            fields: a list of strings representing all fields to obfuscate
+                    ["password", "date_of_birth"]
+            redaction: a string representing by what the
+                       field will be obfuscated
+                       "XXXXX"
+            message: a string representing the log line
+                    ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;"]
+                    ["name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
+            separator: a string representing by which character is
+                    separating all fields in the log line (message)
+                    ";"
+        Return:
+            String with string ofuscated
     """
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
