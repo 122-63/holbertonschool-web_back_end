5e41a29a5ed8ddd2df685e0b39b3b5d5769fcd07
122-63 <3469@holbertonschool.com>
2022-07-06 23:25:19 -0500

    Parameterize and patch

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,8 @@ Parameterize a unit test
 """
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json, memoize
+from unittest.mock import patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -14,7 +15,7 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """
@@ -32,3 +33,43 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
         # print("\n ERROR",error.exception.args)
         self.assertEqual(error.exception.args[0], path[-1])
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    testing get
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('test_utils.get_json')
+    def test_get_json(self, test_url, test_payload, mock):
+        """ mock calls"""
+        mock.return_value = test_payload
+        result = mock.get_json(test_url)
+        self.assertEqual(result, mock.return_value)
+
+
+class TestMemoize(unittest.TestCase):
+    """ Test Class to memoize """
+
+    def test_memoize(self):
+        """ Test memoize """
+        class TestClass:
+            """ Test Class """
+
+            def a_method(self):
+                """ A method """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """ Decorator """
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()
