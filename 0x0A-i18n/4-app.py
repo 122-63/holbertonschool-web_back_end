318f34957c6c2706f9bf7eee1b9360374e5c21ad
122-63 <3469@holbertonschool.com>
2022-07-07 22:03:13 -0500

    Force locale with URL parameter

--- 0x0A-i18n/4-app.py
+++ 0x0A-i18n/4-app.py
@@ -1,43 +1,38 @@
 #!/usr/bin/env python3
-""" Basic Babel setup """
-from flask import Flask, render_template, request
-from flask_babel import Babel, _
 
+"""Basic Flask app w/ Babel extension"""
 
-class Config(object):
-    """ Configuration Babel """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-    BABEL_DEFAULT_LOCALE = 'en'
-
+from flask import Flask, render_template, request, flash
+from flask_babel import Babel
 
 app = Flask(__name__, template_folder='templates')
-app.config.from_object(Config)
 babel = Babel(app)
+
+
+class Config():
+    """Configuration class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def hello_world() -> str:
+    """Renders 4-index template with Hello World"""
+    return render_template("4-index.html")
 
 
 @babel.localeselector
 def get_locale():
-    """ Locale language
-        Return:
-            Best match to the language
-    """
-    locale = request.args.get('locale', None)
-
-    if locale and locale in app.config['LANGUAGES']:
+    """Gets locale from a best match"""
+    locale = request.args.get('locale')
+    if locale in Config.LANGUAGES:
         return locale
-
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def hello_world():
-    """ Greeting
-        Return:
-            Initial template html
-    """
-    return render_template('4-index.html')
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run()
