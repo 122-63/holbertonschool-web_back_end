482d67467956b94a41172ab1c6c18892546a7e92
122-63 <3469@holbertonschool.com>
2022-08-04 12:17:51 -0500

    Methods, static methods

---
+++ 0x10-ES6_classes/3-currency.js
@@ -0,0 +1,36 @@
+/*
+Methods, static methods
+return the attributes in the following format name (code)
+*/
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== "string") {
+      throw new TypeError("Code must be a string");
+    }
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== "string") {
+      throw new TypeError("Name must be a string");
+    }
+    this._name = value;
+  }
+}
